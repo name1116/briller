74910276f4e34a1baa8bedd7613dc25a13472cd7
name1116 <milk100409@gmail.com>
2024-11-15 17:34:38 +0900

    작업 전 템플릿 푸시

---
+++ components/pagination.html
@@ -0,0 +1,23 @@
+<div class="pagination col-group">
+    <a href="" class="page-btn">
+        <i class="xi-angle-left-min"></i>
+    </a>
+    <a href="" class="page-btn active">
+        1
+    </a>
+    <a href="" class="page-btn">
+        2
+    </a>
+    <a href="" class="page-btn">
+        3
+    </a>
+    <a href="" class="page-btn">
+        4
+    </a>
+    <a href="" class="page-btn">
+        5
+    </a>
+    <a href="" class="page-btn">
+        <i class="xi-angle-right-min"></i>
+    </a>
+</div>
---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function(){
+    //헤더 푸터 컴포넌트
+    $('#header').load('../components/header.html');
+    $('#footer').load('../components/footer.html');
+
+    $('#pagination').load('../components/pagination.html');
+    $('#top_menu').load('../components/top_menu.html');
+})
+
+/*smooth-scroll*/
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollTrigger.normalizeScroll(true)
+
+// create the smooth scroller FIRST!
+let smoother = ScrollSmoother.create({
+  smooth: 2,
+  effects: true,
+  normalizeScroll: true
+});
+
+// pin box-c when it reaches the center of the viewport, for 300px
+ScrollTrigger.create({
+  trigger: ".box-c",
+  pin: true,
+  start: "center center",
+  end: "+=300",
+  markers: true
+});
+
+document.querySelector("button").addEventListener("click", e => {
+  // scroll to the spot where .box-c is in the center.
+  // parameters: element, smooth, position
+  smoother.scrollTo(".box-c", true, "center center");
+  
+  // or you could animate the scrollTop:
+  // gsap.to(smoother, {
+  //  scrollTop: smoother.offset(".box-c", "center center"),
+  //  duration: 1
+  // });
+});
+import { GSAPInfoBar } from "https://codepen.io/GreenSock/pen/vYqpyLg.js"
+new GSAPInfoBar({ link: "https://gsap.com/docs/v3/Plugins/ScrollSmoother/"});
