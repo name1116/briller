3ba59e19d987cc2bee3744cbd19a1991df03a5c0
name1116 <milk100409@gmail.com>
2024-11-18 16:53:55 +0900

    header, 메인 페이지, top-menu 수정중

--- js/script.js
+++ js/script.js
@@ -8,37 +8,37 @@ $(document).ready(function(){
     $('#top_menu').load('../components/top_menu.html');
 })
 
-/*smooth-scroll*/
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// /*smooth-scroll*/
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollTrigger.normalizeScroll(true)
+// ScrollTrigger.normalizeScroll(true)
 
-// create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-  smooth: 2,
-  effects: true,
-  normalizeScroll: true
-});
+// // create the smooth scroller FIRST!
+// let smoother = ScrollSmoother.create({
+//   smooth: 2,
+//   effects: true,
+//   normalizeScroll: true
+// });
 
-// pin box-c when it reaches the center of the viewport, for 300px
-ScrollTrigger.create({
-  trigger: ".box-c",
-  pin: true,
-  start: "center center",
-  end: "+=300",
-  markers: true
-});
+// // pin box-c when it reaches the center of the viewport, for 300px
+// ScrollTrigger.create({
+//   trigger: ".box-c",
+//   pin: true,
+//   start: "center center",
+//   end: "+=300",
+//   markers: true
+// });
 
-document.querySelector("button").addEventListener("click", e => {
-  // scroll to the spot where .box-c is in the center.
-  // parameters: element, smooth, position
-  smoother.scrollTo(".box-c", true, "center center");
+// document.querySelector("button").addEventListener("click", e => {
+//   // scroll to the spot where .box-c is in the center.
+//   // parameters: element, smooth, position
+//   smoother.scrollTo(".box-c", true, "center center");
   
-  // or you could animate the scrollTop:
-  // gsap.to(smoother, {
-  //  scrollTop: smoother.offset(".box-c", "center center"),
-  //  duration: 1
-  // });
-});
-import { GSAPInfoBar } from "https://codepen.io/GreenSock/pen/vYqpyLg.js"
-new GSAPInfoBar({ link: "https://gsap.com/docs/v3/Plugins/ScrollSmoother/"});
+//   // or you could animate the scrollTop:
+//   // gsap.to(smoother, {
+//   //  scrollTop: smoother.offset(".box-c", "center center"),
+//   //  duration: 1
+//   // });
+// });
+// import { GSAPInfoBar } from "https://codepen.io/GreenSock/pen/vYqpyLg.js"
+// new GSAPInfoBar({ link: "https://gsap.com/docs/v3/Plugins/ScrollSmoother/"});
